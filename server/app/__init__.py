a79eb811367c386526227cc74e95cbafd579f0dd
izzie <izziedev254@gmail.com>
2025-04-26 23:36:39 +0300

    created flask app instance in run.py

---
+++ server/app/__init__.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+from .config import Config
+
+#initializing extensions without app context
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+bcrypt = Bcrypt()
+ma = Marshmallow()
+cors = CORS()
+api = Api()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    #initializinbg extensions with the app instance
+    db.init_app(app)
+    migrate.init_app(app,db)
+    jwt.init_app(app)
+    bcrypt.init_app(app)
+    ma.init_app(app)
+    cors.init_app(app, resources={r"/api/*": {"origins": "*"}})
+    api.init_app(app, prefix="/api")
+
+    #return the app
+    @app.route('/')
+    def index():
+        return "Shoply Backend is running!"
+
+    return app
+
