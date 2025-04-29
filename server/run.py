1d0d65f4cee21c757d071f454f261c93e75575c6
isaac kyalo <kyalomutie@students.ku.ac.ke>
2025-04-29 11:55:22 +0300

    add host & port options with default values

--- server/run.py
+++ server/run.py
@@ -1,7 +1,8 @@
+import os
 from app import create_app
 
 #creating flask app instance 
 app = create_app()
 
 if __name__ =='__main__':
-    app.run(debug=True)
+    app.run(debug=True, host=os.getenv("host", "0.0.0.0"), port=os.getenv("port", 5000))
